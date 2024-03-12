cd37d1551573e2e8d38e9b7b78f52e4a4afdab8b
burhan <9burhanjehloh@gmail.com>
2024-03-12 14:13:32 +0700

    update-massage

--- controllers/update-message.go
+++ controllers/update-message.go
@@ -44,6 +44,14 @@ func MessageUpdateStatus(message []byte) error {
 		}
 
 	} else if strings.Contains(string(message), "Fan") {
+		// var body UpdateStatusFan
+		// err := json.Unmarshal(message, &body)
+		// if err != nil {
+		// 	log.Println("Error:", err)
+		// 	return err
+		// }
+		// log.Println(body)
+		// initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Fan, body.Client)
 		var body UpdateStatusFan
 		err := json.Unmarshal(message, &body)
 		if err != nil {
@@ -51,8 +59,24 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Fan, body.Client)
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Fan, body.Client)
+		if err != nil {
+			panic(err)
+		}
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
+		if err != nil {
+			panic(err)
+		}
+
 	} else if strings.Contains(string(message), "Gate") {
+		// var body UpdateStatusGate
+		// err := json.Unmarshal(message, &body)
+		// if err != nil {
+		// 	log.Println("Error:", err)
+		// 	return err
+		// }
+		// log.Println(body)
+		// initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Gate, body.Client)
 		var body UpdateStatusGate
 		err := json.Unmarshal(message, &body)
 		if err != nil {
@@ -60,8 +84,24 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Gate, body.Client)
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Gate, body.Client)
+		if err != nil {
+			panic(err)
+		}
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
+		if err != nil {
+			panic(err)
+		}
+
 	} else if strings.Contains(string(message), "PWM") {
+		// var body UpdateStatusPWM
+		// err := json.Unmarshal(message, &body)
+		// if err != nil {
+		// 	log.Println("Error:", err)
+		// 	return err
+		// }
+		// log.Println(body)
+		// initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.PWM, body.Client)
 		var body UpdateStatusPWM
 		err := json.Unmarshal(message, &body)
 		if err != nil {
@@ -69,7 +109,14 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.PWM, body.Client)
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.PWM, body.Client)
+		if err != nil {
+			panic(err)
+		}
+		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	return nil
