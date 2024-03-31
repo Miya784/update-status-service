e860f03fd874f0d47016df22af9e23c6b565ed69
Miya784 <139578102+Miya784@users.noreply.github.com>
2024-03-31 14:39:59 +0700

    Update update-message.go

--- controllers/update-message.go
+++ controllers/update-message.go
@@ -26,13 +26,6 @@ type UpdateStatusPWM struct {
 }
 
 func MessageUpdateStatus(message []byte) error {
-	// Check if message is a valid JSON
-	var validJSON map[string]interface{}
-	if err := json.Unmarshal(message, &validJSON); err != nil {
-		log.Println("Error: Invalid JSON format")
-		return err
-	}
-
 	if strings.Contains(string(message), "Light") {
 		var body UpdateStatusLight
 		err := json.Unmarshal(message, &body)
@@ -58,15 +51,7 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Fan, body.Client)
-		if err != nil {
-			panic(err)
-		}
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
-		if err != nil {
-			panic(err)
-		}
-
+		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Fan, body.Client)
 	} else if strings.Contains(string(message), "Gate") {
 		var body UpdateStatusGate
 		err := json.Unmarshal(message, &body)
@@ -75,15 +60,7 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Gate, body.Client)
-		if err != nil {
-			panic(err)
-		}
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
-		if err != nil {
-			panic(err)
-		}
-
+		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.Gate, body.Client)
 	} else if strings.Contains(string(message), "PWM") {
 		var body UpdateStatusPWM
 		err := json.Unmarshal(message, &body)
@@ -92,14 +69,7 @@ func MessageUpdateStatus(message []byte) error {
 			return err
 		}
 		log.Println(body)
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.PWM, body.Client)
-		if err != nil {
-			panic(err)
-		}
-		_, err = initials.DB.Exec("UPDATE \"Client\" SET \"updatedAt\" = CURRENT_TIMESTAMP WHERE client = $1", body.Client)
-		if err != nil {
-			panic(err)
-		}
+		initials.DB.Exec("UPDATE \"Client\" SET status = $1 WHERE client = $2", body.PWM, body.Client)
 	}
 
 	return nil
